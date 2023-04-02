b32e7c41e645e50b7bc5767ec2da4368cf7c6cd9
Roman <im.roman996@gmail.com>
2023-04-02 16:15:14 +0300

    Renamed project

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 
 app.use(express.json());
 
-const games = [
+const items = [
     {id:1, name: "Witcher 3", price: 29.99},
     {id:2, name: "Cyberpunk 2077", price: 59.99},
     {id:3, name: "Minecraft", price: 26.99},
@@ -19,42 +19,42 @@ const games = [
     {id:9, name: "League of Legends", price: 0.00}
 ]
 
-app.get('/games', (req, res) => {
+app.get('/items', (req, res) => {
 
-    if (typeof games[req.params.id -1] === 'undefined'){
-        return res.status(404).send({error: "Game not found"})
+    if (typeof items[req.params.id -1] === 'undefined'){
+        return res.status(404).send({error: "Item not found"})
     }
 
-    res.send(games)
+    res.send(items)
 })
 
-app.get('/games/:id', (req, res) => {
-    res.send(games[req.params.id -1])
+app.get('/items/:id', (req, res) => {
+    res.send(items[req.params.id -1])
 })
 
-app.post('/games/:id', (req, res) => {
+app.post('/items/:id', (req, res) => {
     if (!req.body.name || !req.body.price){
         return res.status(400).send({error: 'One or all params are missing'})
     }
-    let game = {
-        id: games.length +1,
+    let item = {
+        id: items.length +1,
         price: req.body.price,
         name: req.body.name
     }
 
-    games.push(game)
+    items.push(item)
     
     res.status(201)
-        .location(`${getBaseUrl(req)}/games/${games.lenght}`)
-        .send(game)
+        .location(`${getBaseUrl(req)}/items/${items.lenght}`)
+        .send(item)
 })
 
-app.delete('/games/:id', (req, res) =>{
-    if (typeof games[req.params.id -1]=== 'undefined'){
-        return res.status(404).send({error:"Game not found"})
+app.delete('/items/:id', (req, res) =>{
+    if (typeof items[req.params.id -1]=== 'undefined'){
+        return res.status(404).send({error:"Item not found"})
     }
 
-    games.splice(req.params.id -1, 1)
+    items.splice(req.params.id -1, 1)
 
     res.status(204).send({error: "No content"})
 })
